f901f5456ed295994e82d791495fbd5b9ab2d84d
Jonas Lindner <lindnjon@ouga15.cmm.in.tum.de>
2022-12-04 16:37:31 +0100

    write clumping variants as Int8

--- create_parquet.py
+++ create_parquet.py
@@ -5,9 +5,14 @@ import pyarrow.parquet as pq
 
 csv_file = sys.argv[1]
 parquet_file = sys.argv[2]
-chunksize = 10000
 
-csv_stream = pd.read_csv(csv_file, sep='\t', chunksize=chunksize, low_memory=False)
+col_names = pd.read_csv(csv_file, sep="\t",nrows=0).columns
+
+non_variant_mac_columns = {"FID", "IID", "PAT", "MAT", "SEX", "PHENOTYPE"}
+types_dict = {col: "Int8" for col in col_names if col not in non_variant_mac_columns}
+
+chunksize = 10000
+csv_stream = pd.read_csv(csv_file, sep='\t', chunksize=chunksize, dtype=types_dict)
 
 for i, chunk in enumerate(csv_stream):
     print("Chunk", i)
